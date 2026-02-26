3b0ea5bf605e05486e0f33400a0462164d2ef4aa
Javier Mejia <javiermejia@cervezacadejo.com>
2026-02-26 12:01:11 -0600

    fix: prefijo de rutas cambiado a pagos

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\Api\Finanzas\SolicitudPagoDetalleController;
 use App\Http\Controllers\Api\Finanzas\SolicitudPagoAdjuntoController;
 use App\Http\Controllers\Api\Finanzas\PresupuestoUnidadController;
 
-// Finanzas: Catálogos
-Route::prefix('finanzas')->group(function () {
+// Pagos: Catálogos y operaciones
+Route::prefix('pagos')->group(function () {
         Route::post('solicitudes-pago/preview', [SolicitudPagoController::class, 'preview']);
     Route::get('contribuyentes', [CatalogosFinanzasController::class, 'contribuyentes']);
     Route::get('formas-pago', [CatalogosFinanzasController::class, 'formasPago']);
