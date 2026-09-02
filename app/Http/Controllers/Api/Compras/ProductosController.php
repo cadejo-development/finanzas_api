e298876473bf07b11386765858521070944217e4
Javier Mejia <javiermejia@cervezacadejo.com>
2026-09-02 16:28:43 -0600

    Revert "fix: solo_mp no excluia categorias MP-03/MP-06+ que son alimentos (ej. Panko)"
    This reverts commit 5db763635e96f6056f1f7ddf33af030325063ed1.

--- app/Http/Controllers/Api/Compras/ProductosController.php
+++ app/Http/Controllers/Api/Compras/ProductosController.php
@@ -86,13 +86,12 @@ class ProductosController extends Controller
             $query->where('origen', $origen);
         }
 
-        // solo_mp=1: excluye platos del menú y materias primas exclusivas de cervecería
-        // PL-01/05/07/08/12/18 = platos del menú; MP-01/02/04/05 = aditivos/cereales/levadura/lúpulo
-        // NO excluir MP-03/MP-06/etc. que son alimentos generales (ej. Materia Prima Alimentos)
+        // solo_mp=1: excluye platos del menú y materiales de fábrica de cerveza
+        // Se excluyen PL-01/05/07/08/12/18 (platos del menú) pero NO PL-20 (sub-recetas/CPs que sí son MP)
         if ($request->boolean('solo_mp')) {
             $query->whereDoesntHave('categoria', fn ($q) =>
-                $q->whereIn('key', ['PL-01', 'PL-05', 'PL-07', 'PL-08', 'PL-12', 'PL-18',
-                                    'MP-01', 'MP-02', 'MP-04', 'MP-05'])
+                $q->whereIn('key', ['PL-01', 'PL-05', 'PL-07', 'PL-08', 'PL-12', 'PL-18'])
+                  ->orWhere('key', 'ilike', 'MP-%')
             );
         }
 
